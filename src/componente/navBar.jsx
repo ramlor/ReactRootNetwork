91154c829c5601a30f4723866395cc21f6790c8f
lorena <lorena.ramos@itscordoba.edu.ar>
2024-10-03 21:59:00 -0300

    guardando cambios

---
+++ src/componente/navBar.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+
+export default function navBar() {
+  return (
+    <>
+    
+    <div className='saludo'>¿HOLA mundo!</div>
+    </>
+    
+  )
+}
+
+export default navBar;

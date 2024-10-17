456b11cb102e7351e85dacc9d6e5736ff7c7c117
lorena <lorena.ramos@itscordoba.edu.ar>
2024-10-17 18:39:29 -0300

    subido

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 
-import './App.css';
-import { navBar} from './componente/navBar/navBar'
+//import './App.css';
+//import navBar from './componente/navBar/navBar'
 
-function App() {
-  return (
-    <div className="App">
-      <navBar><navBar/>
+//function App() {
+// return (
+  //  <div className="App">
+    //  <navBar><navBar/>
       
-    </div>
-  );
+    //</div>
+  //);
+//}
+
+//export default App;
+//import { useState } from 'react';
+import MyInput from './componente/MyInput/MyInput';
+
+function App(){
+  const sumar = () =>{
+    setCount (count + 1);
+  }
+
+
+  const restar = () =>{
+    setCount (count - 1);
+  }
+
+  const [count, setCount] = useState(1);
+
+  return (
+   <>
+      <NavBar><NavBar/>
+      <MyButton text= {"Sumar"} callback = {sumar}>variant ={"primary"}<MyButton/>
+      <MyButton text= {"Restar"} callback = {restar}>variant ={"secondary"}<MyButton/>
+      <MyInput count= {count}> </MyInput>
+    </>
+  );  
 }
 
-export default App;
+export default App ;
---
+++ src/componente/MyInput/MyInput.jsx
@@ -0,0 +1,4 @@
+import 'bootstrap/dis/css/bootstrap.min.css'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
